5bbfbf03e8eb5e9b215798726bc774473bf8943d
Javi <javigonro10@gmail.com>
2025-02-03 11:10:39 +0100

    prueba 2

--- calculadora.js
+++ calculadora.js
@@ -8,5 +8,14 @@ function sumar(a, b) {
     return a - b;
   }
 
+  function multiplicar(a, b) {
+    return a * b;
+  }
+
+  function dividir(a, b) {
+    return a / b;
+  }
+  
+
 
   module.exports = { sumar, restar };
--- calculadora.test.js
+++ calculadora.test.js
@@ -7,3 +7,11 @@ test('Suma 2 + 3 y devuelve 5', () => {
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
 });
+
+test ('Multiplica 2 * 3 y devuelve 6', () => {
+    expect(multiplicar(2, 3)).toBe(6);
+});
+
+test ('Divide 6 / 3 y devuelve 2', () => {
+    expect(dividir(6, 3)).toBe(2);
+});
